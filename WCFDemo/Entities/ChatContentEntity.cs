09cbd4677b534da41e3d38022a9db1fad883c0f6
Mark Wu <wuweiwei@skieer.com>
2017-07-10 18:29:31 +0800

    IPC part1

---
+++ WCFDemo/Entities/ChatContentEntity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WCFDemo.Entities
+{
+    public class ChatContentEntity:AIPCEntity
+    {
+        public string TextMessage { get; set; }
+        public List<Image> ImagesMessage { get; set; }
+    }
+}
