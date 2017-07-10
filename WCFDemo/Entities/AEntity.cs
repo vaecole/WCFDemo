03f6618f5a310cae689174e19392221d4bd2906c
Mark Wu <wuweiwei@skieer.com>
2017-07-10 18:29:31 +0800

    IPC part1

---
+++ WCFDemo/Entities/AEntity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+
+namespace WCFDemo.Entities
+{
+    public abstract class AIPCEntity
+    {
+        public string Message { get; set; }
+        public string Code { get; set; }
+    }
+}
---
+++ WCFDemo/IPC/IIPC.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WCFDemo.Entities;
+
+namespace WCFDemo.IPC
+{
+    public interface IIPC
+    {
+        bool Hi();
+        AIPCEntity SendChatMSG(AIPCEntity entity);
+    }
+}
