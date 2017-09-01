8651960b82c289c892f1078abd8ad5ab89951e34
Mark Wu <wuweiwei@skieer.com>
2017-09-01 14:54:16 +0800

    Ever2Datable Tool

---
+++ Ever2Datable/Ever2Datable.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ever2Datable
+{
+    public class Converter<T>
+    {
+        public static DataTable Convert2(IEnumerable<T> entities)
+        {
+            if (entities == null || entities.Count() == 0)
+            {
+                return null;
+            }
+
+            Type type = entities.FirstOrDefault().GetType();
+            var properties = type.GetProperties();
+            if (properties == null || properties.Count() == 0)
+            {
+                return null;
+            }
+
+            var columns = properties.Select(s => new DataColumn(s.Name));
+            DataTable dt = new DataTable(type.Name, type.Namespace);
+            dt.Columns.AddRange(columns.ToArray());
+
+            foreach (var item in entities)
+            {
+                var newRow = dt.NewRow();
+                foreach (var property in properties)
+                {
+                    newRow[property.Name] = property.GetValue(item);
+                }
+                dt.Rows.Add(newRow);
+            }
+            return dt;
+        }
+    }
+}
---
+++ WCFDemo/Entities/HttpProxyEntity.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Octopus.API.SDK.Entites
+{
+    public class HttpProxyEntity
+    {
+        public string IP { get; set; }
+        public int Port { get; set; }
+        public DateTime ExpirationTime { get; set; }
+
+        public string ProxyId { get; set; }
+
+        public bool WillExpireIn(int milliSeconds = 60 * 1000)
+        {
+            return ExpirationTime - DateTime.Now <= TimeSpan.FromMilliseconds(milliSeconds);
+        }
+
+        public double ValidMilliSeconds
+        {
+            get
+            {
+                return (ExpirationTime - DateTime.Now).TotalMilliseconds;
+            }
+        }
+
+        public override string ToString()
+        {
+            return IP + ":" + Port.ToString();
+        }
+    }
+    public enum HttpProxyMode
+    {
+        None = 0,
+        Manual = 1,
+        Auto = 2
+    }
+
+    public enum HttpProxyStartStatus
+    {
+        OK,
+        NetworkDown,
+        UserNotAllowed,
+        ProxyServiceInvalid,
+        InvalidConfig,
+        Unknown
+    }
+}
