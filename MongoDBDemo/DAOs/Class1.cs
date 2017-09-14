616a6c28e482494f299dcca57fa52580d8e1c858
Mark Wu <wuweiwei@skieer.com>
2017-09-14 19:03:17 +0800

    新增AutoChromeDemo项目

---
+++ MongoDBDemo/DAOs/Class1.cs
@@ -0,0 +1,23 @@
+﻿using MongoDB.Driver;
+using MongoDB.Driver.Builders;
+using MongoDBDemo.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MongoDBDemo.DAOs
+{
+    public class RequestLogRepository : StandardMongoDataAccessor<RequestLog>
+    {
+        public RequestLogRepository(string dbserver) : base("RequestLog", dbserver, "ApiLogs")
+        { }
+
+        public List<RequestLog> QueryLogFrom2(DateTime startTime, DateTime endTime)
+        {
+            IMongoQuery query = Query<RequestLog>.Where(e => e.CreatedDate >= startTime && e.CreatedDate <= endTime);
+            return GetList(query);
+        }
+    }
+}
---
+++ MongoDBDemo/Entities/ApiUseLog.cs
@@ -0,0 +1,24 @@
+﻿using MongoDB.Driver;
+using MongoDB.Driver.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MongoDBDemo.Entities
+{
+    [CollectionName("RequestLog")]
+    public class RequestLog : MongoEntity
+    {
+        public string UserId { get; set; }
+
+        public string ApiId { get; set; }
+
+        public string Url { get; set; }
+
+        public string Paramaters { get; set; }
+
+        public DateTime CreatedDate { get; set; }
+    }
+}
