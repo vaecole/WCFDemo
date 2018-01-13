72f7ff289b77fb85aa2827c4f527e6c4fd8760f7
Mark Wu <wuweiwei@skieer.com>
2018-01-14 01:24:58 +0800

    delete private nuget

--- WCFDemo/MyTaskScheduler.cs
+++ WCFDemo/MyTaskScheduler.cs
@@ -1,11 +1,7 @@
-﻿using Octopus.Utility;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
-using System.Timers;
 
 namespace WCFDemo
 {
